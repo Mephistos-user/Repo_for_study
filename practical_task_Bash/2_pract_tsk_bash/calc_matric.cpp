952fa0320d5980fe49c82f30dd17e151269719ae
Mephistos-user <Mephistos.user@yandex.ru>
2023-06-01 23:06:16 +0300

    обновление калькулятора матриц

--- practical_task_Bash/2_pract_tsk_bash/calc_matric.cpp
+++ practical_task_Bash/2_pract_tsk_bash/calc_matric.cpp
@@ -3,43 +3,44 @@ using namespace std;
 // задаем переменные размера матриц
 int row_1, column_1, row_2, column_2;
 
+// функция определения размеров исходных матриц:
+void sizeMatric() {
+    cout << "Введите количество строк и столбцов первой матрицы: " << endl;
+    cin >> row_1 >> column_1;
 
-
+    cout << "Введите количество строк и столбцов второй матрицы: " << endl;
+    cin >> row_2 >> column_2;
+}
+// функция проверки ошибки размеров матриц:
+void sizeError() {
+    while (column_1 != row_2 ) {
+        cout << "Неправильный размер матриц!\nПеремножение невозможно!\n(Количество столбцов матрицы1 должно быть равно количеству строк матрицы2)\nВвести размеры повторно?(y/n)" << endl;
+        string answ;
+        cin >> answ;
+        if(answ=="y"){
+            // ввод размеров матриц повторно
+            sizeMatric();
+        } else exit(0);
+    }
+}
 
 int main() {
 
-//public void sizeMatric(){
-cout << "Введите количество строк и столбцов первой матрицы: " << endl;
-cin >> row_1 >> column_1;
-
-cout << "Введите количество строк и столбцов второй матрицы: " << endl;
-cin >> row_2 >> column_2;
-//}
-if(column_1 != row_2 ) {
-    //string answ;
-    cout << "Неправильный размер матриц!\nПеремножение невозможно!\n(Количество столбцов матрицы1 должно быть равно количеству строк матрицы2)\nВвести размеры повторно?(y/n)" << endl;
-    /*cin >> answ;
-    if(answ=="y"){
-        // ввод размеров матриц повторно
-        sizeMatric();
-    }*/
-}
-
-//cout << row_1 << " " << column_1 << endl;
-
-//cout << row_2 << " " << column_2 << endl;
+sizeMatric();
+sizeError();
 
 //задаем размеры матриц на основе введенных данных
 int arr_1[row_1][column_1], arr_2[row_2][column_2], arr_3[row_1][column_2];
 
 // вводим значения в матрицу 1:
+cout << "Введите " << row_1*column_1 << " значений матрицы 1:" << endl;
 for (int i=0;i<row_1;++i){
     for(int j=0;j<column_1;++j){
         cin >> arr_1[i][j];
     }
 }
-
 // вводим значения в матрицу 2:
+cout << "Введите " << row_2*column_2 << " значений матрицы 2:" << endl;
 for (int i=0;i<row_2;++i){
     for(int j=0;j<column_2;++j){
         cin >> arr_2[i][j];
@@ -53,7 +54,6 @@ for (int i=0;i<row_1;++i){
     }
     cout << endl;
 }
-
 // выводим значения из матрицы 2:
 cout << "матрица 2:" << endl;
 for (int i=0;i<row_2;++i){
@@ -68,10 +68,7 @@ for(int i=0;i < row_1; ++i){
             arr_3[i][j]=0;
     }  
 }
-
-
 // вычисляем и записываем значения в матрицу3
-
 for(int i=0;i < row_1; ++i){
     for (int j=0;j<column_2; ++j){
         for (int n=0; n<column_1; ++n){
@@ -79,7 +76,6 @@ for(int i=0;i < row_1; ++i){
         }
     }
 }
-
 // выводим значения из матрицы 3:
 cout << "матрица 3:" << endl;
 for (int i=0;i<row_1;++i){
@@ -88,6 +84,5 @@ for (int i=0;i<row_1;++i){
     }
     cout << endl;
 }
-
     return 0;
 }
