8dbee446a0daf8b07eb23a03e94e527ac1d02c6f
Mephistos-user <Mephistos.user@yandex.ru>
2023-06-03 19:51:30 +0300

    fiks

--- practical_task_Bash/2_pract_tsk_bash/calc_matric_multiproc.cpp
+++ practical_task_Bash/2_pract_tsk_bash/calc_matric_multiproc.cpp
@@ -38,44 +38,50 @@ int arr_1[row_1][column_1], arr_2[row_2][column_2], arr_3[row_1][column_2];
 
 // вводим значения в матрицу 1:
 cout << "Введите " << row_1*column_1 << " значений матрицы 1:" << endl;
-for (int i=0;i<row_1;++i){
-    for(int j=0;j<column_1;++j){
+for (int i=0;i<row_1;++i)
+{
+    for(int j=0;j<column_1;++j)
+    {
         cin >> arr_1[i][j];
     }
 }
 // вводим значения в матрицу 2:
 cout << "Введите " << row_2*column_2 << " значений матрицы 2:" << endl;
-for (int i=0;i<row_2;++i){
-    for(int j=0;j<column_2;++j){
+for (int i=0;i<row_2;++i)
+{
+    for(int j=0;j<column_2;++j)
+    {
         cin >> arr_2[i][j];
     }
 }
 // выводим значения из матрицы 1:
 cout << "матрица 1:" << endl;
-for (int i=0;i<row_1;++i){
-    for(int j=0;j<column_1;++j){
+for (int i=0;i<row_1;++i)
+{
+    for(int j=0;j<column_1;++j)
+    {
         cout << arr_1[i][j] << " ";
     }
     cout << endl;
 }
 // выводим значения из матрицы 2:
 cout << "матрица 2:" << endl;
-for (int i=0;i<row_2;++i){
-    for(int j=0;j<column_2;++j){
+for (int i=0;i<row_2;++i)
+{
+    for(int j=0;j<column_2;++j)
+    {
         cout << arr_2[i][j] << " ";
     }
     cout << endl;
 }
-// обнуляем значения матрицы 3
-for(int i=0;i < row_1; ++i){
-    for (int j=0;j<column_2; ++j){
-            arr_3[i][j]=0;
-    }  
-}
+
 // вычисляем и записываем значения в матрицу3
 for(int i=0;i < row_1; ++i){
     for (int j=0;j<column_2; ++j){
         for (int n=0; n<column_1; ++n){
+            // обнуляем значения матрицы 3
+            arr_3[i][j]=0;
+            // записываем значения
             arr_3[i][j]=arr_3[i][j]+arr_1[i][n]*arr_2[n][j];
         }
     }
