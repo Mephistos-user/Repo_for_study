5627b5000aa2f80a47165e6edd14a38d0454eb04
Mephistos-user <Mephistos.user@yandex.ru>
2023-07-23 11:27:32 +0300

    обавление вектора потоков и работа с переменными

--- practical_task_Bash/5_pract_tsk_bash/test.cpp
+++ practical_task_Bash/5_pract_tsk_bash/test.cpp
@@ -10,26 +10,41 @@ using namespace std;
 
 
 
-void calc()
+void calc(int &args, int arg)
 {
-    cout << "Test message" << endl;
-    this_thread::sleep_for(chrono::milliseconds(7000));
+    this_thread::sleep_for(chrono::milliseconds(3000));
+    cout << "ID потока = " << this_thread::get_id() << "\tthread\t" << arg << endl;
+    cout << "Test message " << args << endl;
+    
 }
 
 int main ()
 {
     setlocale (LC_ALL, "ru");
-    
-        thread thrd(calc);
-    
+        //определить колличество ядер
+    //may return 0 when not able to detect
+    const auto num_cores = thread::hardware_concurrency();
+    cout << "Колличество доступных ядер num_cores = " << num_cores << endl;
+
+
+    //thread thrd(calc);
+    vector <thread> thrd;
+    for(int t = 1; t <= num_cores; t++)
+    {
+        thrd.push_back (thread(calc, (ref(t)), t));
+    }
     
     
 
     this_thread::sleep_for(chrono::milliseconds(5000));
-
+    cout << "ID потока = " << this_thread::get_id() << "\tmain\t" << endl;
     cout << "Message from main" << endl;
     
-    thrd.join();
+    for(int t = 0; t < num_cores; ++t)
+    {    
+        thrd.at (t).join ();
+    }
+    //thrd.join();
     
     return 0;
 }
