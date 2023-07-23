88082b2c9410d50e71f5bff0f0623a4f5f62d359
Mephistos-user <Mephistos.user@yandex.ru>
2023-07-23 17:53:21 +0300

    фиксация

--- practical_task_Bash/5_pract_tsk_bash/test.cpp
+++ practical_task_Bash/5_pract_tsk_bash/test.cpp
@@ -1,50 +1,278 @@
 #include <iostream>
 #include <ctime>
-#include <pthread.h>
+//#include <pthread.h>
 #include <thread>
 #include <vector>
 #include <chrono>
 
 using namespace std;
 
-
-
-
-void calc(int &args, int arg)
+//задать размеры матриц
+// задаем переменные размера матриц и пустиые матрицы на основе вектора векторов
+int row_1, column_1, row_2, column_2, NUM_CORE_CNST, num_core, num_thread, remaining;
+vector <vector<int>> arr_1;
+vector <vector<int>> arr_2;
+vector <vector<int>> arr_3;
+// функция определения размеров исходных матриц:
+void sizeMatric()
 {
-    this_thread::sleep_for(chrono::milliseconds(3000));
-    cout << "ID потока = " << this_thread::get_id() << "\tthread\t" << arg << endl;
-    cout << "Test message " << args << endl;
-    
+    cout << "Введите количество строк и столбцов первой матрицы: " << endl;
+    cin >> row_1 >> column_1;
+
+    cout << "Введите количество строк и столбцов второй матрицы: " << endl;
+    cin >> row_2 >> column_2;
+    //задаем размеры матриц (векторов) на основе введенных данных
+    arr_1.assign(row_1, vector <int>(column_1));
+    arr_2.assign(row_2, vector <int>(column_2));
+    arr_3.assign(row_1, vector <int>(column_2));
 }
+
+// функция проверки ошибки размеров матриц:
+void sizeError()
+{
+    while (column_1 != row_2 )
+    {
+        cout << "Неправильный размер матриц!\nПеремножение невозможно!\n";
+        cout << "(Количество столбцов матрицы1 должно быть равно количеству строк матрицы2)\n";
+        cout << "Ввести размеры повторно?(y/n)" << endl;
+        char answ;
+        cin >> answ;
+        if(answ=='y')
+        {
+            // ввод размеров матриц повторно
+            sizeMatric();
+        } else exit(0);
+    }
+}
+
+//функция вычисления матрицы (вектора)
+// вычисляем и записываем значения в матрицу 3:
+
+void calc_matrix (int t)
+{
+
+    if (max(row_1,column_2) <= num_core)
+    {
+        if(row_1>=column_2)
+        {
+            //если количество строк больше или равно чем столбцов, то проход по матрице по строкам (t = каждой строке)
+            
+                for (int j=0;j<column_2; ++j)
+                {
+                    for (int n=0; n<column_1; ++n)
+                    {
+                        //arr_3[i][j]=0; // обнуляем значения матрицы 3
+                        arr_3[t-1][j] += arr_1[t-1][n] * arr_2[n][j];
+                    }
+                }
+            
+        }else
+        {
+            //если количество строк меньше чем столбцов, то проход по матрице по столбцам (t = каждому столбцу)
+
+                for (int i=0;i<row_1; ++i)
+                {
+                    for (int n=0; n<column_1; ++n)
+                    {
+                        //arr_3[i][j]=0; // обнуляем значения матрицы 3
+                        arr_3[i][t-1] += arr_1[i][n] * arr_2[n][t-1];
+                    }
+                }
+            
+        }
+
+
+
+
+
+    }else
+    {
+        if(row_1>=column_2)
+        {
+            if(t==num_core)
+            {
+                for(int i=(t-1)*num_thread;i < t*num_thread+remaining; ++i)
+                {
+                    for (int j=0;j<column_2; ++j)
+                    {
+                        for (int n=0; n<column_1; ++n)
+                        {
+                            //arr_3[i][j]=0; // обнуляем значения матрицы 3
+                            arr_3[i][j] += arr_1[i][n] * arr_2[n][j];
+                        }
+                    }
+                }
+            }else
+            {
+                for(int i=(t-1)*num_thread;i < t*num_thread; ++i)
+                {
+                    for (int j=0;j<column_2; ++j)
+                    {
+                        for (int n=0; n<column_1; ++n)
+                        {
+                            //arr_3[i][j]=0; // обнуляем значения матрицы 3
+                            arr_3[i][j] += arr_1[i][n] * arr_2[n][j];
+                        }
+                    }
+                }
+            }
+
+
+
+
+        }else
+        {
+            if(t==num_core)
+            {
+                for(int i=0;i < row_1; ++i)
+                {
+                    for (int j=(t-1)*num_thread;j<t*num_thread+remaining; ++j)
+                    {
+                        for (int n=0; n<column_1; ++n)
+                        {
+                            //arr_3[i][j]=0; // обнуляем значения матрицы 3
+                            arr_3[i][j] += arr_1[i][n] * arr_2[n][j];
+                        }
+                    }
+                }
+            }else
+            {
+                for(int i=0;i < row_1; ++i)
+                {
+                    for (int j=(t-1)*num_thread;j<t*num_thread; ++j)
+                    {
+                        for (int n=0; n<column_1; ++n)
+                        {
+                            //arr_3[i][j]=0; // обнуляем значения матрицы 3
+                            arr_3[i][j] += arr_1[i][n] * arr_2[n][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 int main ()
 {
     setlocale (LC_ALL, "ru");
-        //определить колличество ядер
+    sizeMatric();
+    sizeError();
+    //определить колличество ядер
     //may return 0 when not able to detect
-    const auto num_cores = thread::hardware_concurrency();
-    cout << "Колличество доступных ядер num_cores = " << num_cores << endl;
+    const auto NUM_CORE_CNST = thread::hardware_concurrency();
+    cout << "Колличество доступных потоков NUM_CORE_CNST = " << NUM_CORE_CNST << endl;
+    num_core = NUM_CORE_CNST;
+    //количество строк обрабатываемых в одном потоке
+/*проверяем условие, если количество строк или столбцов результирующей матрицы
+ меньше количества доступных потоков, задаем количество создаваемых новых потоков
+ и направление вычислений (по строкам или столбцам)*/
+if(max(row_1,column_2) <= num_core)
+{
+    num_core = max(row_1,column_2);// количество строк или столбцов обрабатываемых в одном потоке
+    cout << "Колличество создаваемых потоков num_core = " << num_core << endl;
+}else
+{
+    num_thread = max(row_1,column_2)/num_core;
+    cout << "Колличество строк в одном потоке num_thread = " << num_thread << endl;
+    remaining = max(row_1,column_2) - (num_thread*num_core);// остаток строк некратное количеству потоков
+    cout << "Колличество строк в последнем потоке (remaining + num_thread) = " << remaining + num_thread<< endl;
+}
 
 
-    //thread thrd(calc);
-    vector <thread> thrd;
-    for(int t = 1; t <= num_cores; t++)
+cout << "Ввести случайные значения в матрицы (r) или ввести вручную (m)?" << endl;
+char answer;
+cin >> answer;
+if (answer == 'r')
+{
+    // вводим случайные значения в матрицу 1:
+    for (int i = 0; i < row_1; i++) {
+        for (int j = 0; j < column_1; j++) {
+            //arr_1[i][j] = 0;
+            arr_1[i][j] = rand() % 1000;
+        }
+    }
+    // вводим случайные значения в матрицу 2:
+    for (int i = 0; i < row_2; i++) {
+        for (int j = 0; j < column_2; j++) {
+            //arr_2[i][j] = 0;
+            arr_2[i][j] = rand() % 1000;
+        }
+    }
+} else {
+    // вводим значения в матрицу 1 вручную:
+    cout << "Введите " << row_1*column_1 << " значений матрицы 1:" << endl;
+    for (int i=0; i < row_1; ++i) {
+        for(int j=0; j < column_1; ++j) {
+            //arr_1[i][j] = 0;
+            cin >> arr_1[i][j];
+        }
+    }
+    // вводим значения в матрицу 2 вручную:
+    cout << "Введите " << row_2*column_2 << " значений матрицы 2:" << endl;
+    for (int i=0; i < row_2; ++i) {
+        for(int j=0; j < column_2; ++j) {
+            //arr_2[i][j] = 0;
+            cin >> arr_2[i][j];
+        }
+    }
+}
+
+
+
+    // создаем вектор потоков и записываем в него создаваемые потоки   
+    vector <thread> vec_thrd;
+    for(int t = 1; t <= num_core; t++)
     {
-        thrd.push_back (thread(calc, (ref(t)), t));
+        vec_thrd.push_back (thread(calc_matrix, t));
     }
     
+    // выводим значения из матрицы 1:
+    cout << "матрица 1:" << endl;
+    for (int i=0;i<row_1;++i)
+    {
+        for(int j=0;j<column_1;++j)
+        {
+            cout << arr_1[i][j] << " ";
+        }
+        cout << endl;
+    }
+    // выводим значения из матрицы 2:
+    cout << "матрица 2:" << endl;
+    for (int i=0;i<row_2;++i)
+    {
+        for(int j=0;j<column_2;++j)
+        {
+            cout << arr_2[i][j] << " ";
+        }
+        cout << endl;
+    }
+   
+
+
+
+
+    for(int t = 0; t < num_core; ++t)
+    {    
+        vec_thrd.at (t).join ();// ожидаем завершения всех потоков и записи значений в матрицу3
+    }
     
 
-    this_thread::sleep_for(chrono::milliseconds(5000));
-    cout << "ID потока = " << this_thread::get_id() << "\tmain\t" << endl;
-    cout << "Message from main" << endl;
-    
-    for(int t = 0; t < num_cores; ++t)
-    {    
-        thrd.at (t).join ();
+
+
+    // выводим значения из матрицы 3:
+    cout << "Результат умножения - матрица 3:" << endl;
+    for (int i=0;i<row_1;++i) 
+    {
+        for(int j=0;j<column_2;++j)
+        {
+            cout << arr_3[i][j] << " ";
+        }
+        cout << endl;
     }
-    //thrd.join();
+
+
     
     return 0;
 }
