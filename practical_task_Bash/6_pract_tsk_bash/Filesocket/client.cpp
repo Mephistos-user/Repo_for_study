b5f773fd23f82f63f4b56f13694e5c58f99a5397
Mephistos-user <Mephistos.user@yandex.ru>
2023-07-24 23:21:23 +0300

    исходники с багами

---
+++ practical_task_Bash/6_pract_tsk_bash/Filesocket/client.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+
+//#define SOCKET_NAME "/home/mephistos/my_project/6_Sockets/Filesocket/server.soc"
+
+int main()
+{
+    char _soc[5] = ".soc";
+    char SOCKET_NAME [100];
+    scanf("%s", SOCKET_NAME);
+    strncat(SOCKET_NAME, _soc, 5);
+
+    /*if (argc != 2)
+    {
+        printf("./client.out <message>\n");
+        return -1;
+    }*/
+
+    int sfd = socket(AF_UNIX, SOCK_DGRAM, 0);
+    if (sfd == -1)
+    {
+        perror("socket call error");
+        return errno;
+    }
+
+    struct sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+
+    strcpy(addr.sun_path, SOCKET_NAME);
+    printf("sun_path=%s, len=%ld\n", addr.sun_path, strlen(SOCKET_NAME));
+    if (connect(sfd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+    {
+        perror("Can't set dest address");
+        return errno;
+    }
+
+    char *msg = SOCKET_NAME;
+    if (send(sfd, msg, strlen(msg), 0) == -1)
+    {
+        perror("send call error");
+        return errno;
+    }
+    else
+    {
+        printf("send message: %s\n", msg);
+    }
+
+    if (send(sfd, msg, strlen(msg), 0) == -1)
+    {
+        perror("send call error");
+        return errno;
+    }
+    else
+    {
+        printf("send message: %s\n", msg);
+    }
+
+    close(sfd);
+
+    return 0;
+}
