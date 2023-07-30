bd027661a7164ded3be39ec89b31a951d9b77379
Mephistos <Mephistos.user@yandex.ru>
2023-07-30 20:37:51 +0300

    фикс

---
+++ practical_task_python/1_pract_tsk_py/2_task.py
@@ -0,0 +1,20 @@
+print("Введите этапы развития человека по порядку. Начальный этап: ")
+australopithecus = input()
+
+print("Следующий этап")
+homo_habilis = input()
+
+print("Следующий этап")
+homo_erectus = input()
+
+print("Следующий этап")
+neanderthalensis = input()
+
+print("Следующий этап")
+cro_magnon = input()
+
+print("Следующий этап")
+homo_sapiens_sapiens = input()
+
+print("Этапы развития человека:")
+print(australopithecus, homo_habilis, homo_erectus, neanderthalensis, cro_magnon, homo_sapiens_sapiens, sep = "=>")
---
+++ Учеба_питон/1.py
@@ -0,0 +1,14 @@
+print(5)
+print("abc", "qwer")#выводит в одну строку с разделителем через пробел
+print()#пустая строка
+print()
+print("abc", "qwer", sep="-")#sep= заменяет разделитель на указанный в кавычках
+
+print(input())#ввод
+print(input())
+
+x = 8
+x = 9
+print(x)
+
+
