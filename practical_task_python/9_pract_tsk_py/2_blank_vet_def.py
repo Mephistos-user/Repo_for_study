1ce0da79f57e205282697fdb324e21510eb31db1
Mephistos <Mephistos.user@yandex.ru>
2023-08-08 21:57:40 +0300

    fix

--- practical_task_python/9_pract_tsk_py/2_blank_vet_def.py
+++ practical_task_python/9_pract_tsk_py/2_blank_vet_def.py
@@ -19,68 +19,77 @@ def create():#done
     if get_pet(0) != False:
         pets.pop(0)
 
-def read():
+def read():#done
     id = int(input('Введите идентификатор: '))
     if get_pet(id) == False:
         return print(f'Питомца с ID = {id} нет в базе!')
     else:
         pet = dict(get_pet(id))
+        name = ''
+        for key in pet.keys():
+            name = key
+        
+        v = pet[name]['Вид питомца']
+        age = pet[name]['Возраст питомца']
+        h = pet[name]['Имя владельца']
+    
+    print(f'Это {v}, по кличке \"{name}\". Возраст питомца: {age} {get_suffix(age)}. Имя владельца: {h}')
 
-        print(pet)
-        #k = pet.keys
-        #v = pet.values
-
-        #v = pet[k[0]]['Вид питомца']
-        #age = pet[k[0]]['Возраст питомца']
-        #h = pet[k[0]]['Имя владельца']
-    #print(f'Это {v[1]}, по кличке \"{k[0]}\". Возраст питомца: {v[1]}, {get_suffix(v[1])}. Имя владельца: {v[2]}')
-    #print(f'Это {v}, по кличке \"{k[0]}\". Возраст питомца: {age}, {get_suffix(age)}. Имя владельца: {h}')
-
-def update():
+def update():#done
     id = int(input('Введите идентификатор: '))
     if get_pet(id) == False:
         return print(f'Питомца с ID = {id} нет в базе!')
     else:
         name = input('Введите кличку питомца: ')
-    
+        pets[id] = dict()
+        pets[id][name] = dict()
         pets[id][name]['Вид питомца'] = input('Введите вид питомца: ')
 
         pets[id][name]['Возраст питомца'] = int(input('Введите возраст питомца: '))
 
         pets[id][name]['Имя владельца'] = input('Введите имя владельца: ')
-def delete():
+def delete():#done
     id = int(input('Введите идентификатор: '))
     if get_pet(id) == False:
         return print(f'Питомца с ID = {id} нет в базе!')
     else:
         pets.pop(id)
 
-def get_pet(id):
+def get_pet(id):#done
 
-    # функция, с помощью которой вы получите информацию о питомце в виде словаря
-    # сделайте проверку, если питомца с таким ID нету в нашей "базе данных"
-    # верните в этом случае False
-    # а если питомец всё же есть в "базе данных" - верните информацию о нём
-    # выглядеть это может примерно так:
     return pets[id] if id in pets.keys() else False
 
-def get_suffix(age):
-    #year = ''
-    if (age % 10 == 1):
+def get_suffix(age):#done
+    
+    if ((age >= 12) and (age <= 14)):
+        return 'лет'
+    elif (age % 10 == 1):
         return 'год'
     elif ((age % 10 >= 2) and (age % 10 <= 4)):
         return 'года'
     else:
         return 'лет'
 
-
-def pets_list():
+def pets_list():#done
     for i in pets:
         print(get_pet(i))
 
-create()
-create()
-print(pets)
-read()
-pets_list()
+print(f'Список доступных команд:\n\n'
+      f'create - создать новую запись в базе;\n'
+      f'read   - вывести данные о питомце;\n'
+      f'update - обновить запись о питомце;\n'
+      f'delete - удалить запись об питомце;\n'
+      f'pets   - вывести все записи из базы;\n'
+      f'stop   - выйти из программы.\n\n')
 
+command = ''
+
+while command != 'stop':
+    command = input('Введите команду: ')
+    if command == 'create': create()
+    elif command == 'read': read()
+    elif command == 'update': update()
+    elif command == 'delete': delete()
+    elif command == 'pets': pets_list()
+    elif command == 'stop': exit
+    else: print('Не правильная команда!') 
