2c87f0ab594787a555897b04f45c85b2b7c63daa
Mephistos <Mephistos.user@yandex.ru>
2023-08-17 22:38:19 +0300

    fix

--- practical_task_python/Project_helicopter/helicopter.py
+++ practical_task_python/Project_helicopter/helicopter.py
@@ -4,5 +4,18 @@ class Helicopter:
     def __init__(self, w, h):
         rc = randcell(w, h)
         rx, ry = rc[0], rc[1]
+        self.h = h
+        self.w = w
         self.x = rx
         self.y = ry
+        self.tang = 0
+        self.max_tang = 1
+        self.hp = 3
+        self.point = 0
+    def move(self, dx, dy):
+        nx, ny = dx + self.x, dy + self.y
+        if (nx >= 0 and ny >= 0 and nx < self.h and ny < self.w):
+            self.x, self.y = nx, ny
+
+    def print_menu(self):
+        print('🛢️', self.tang, '/', self.max_tang, '💗', self.hp, '💰', self.point)
--- practical_task_python/Project_helicopter/main.py
+++ practical_task_python/Project_helicopter/main.py
@@ -1,7 +1,9 @@
 from map import Map
+from pynput import keyboard
 import time
 import os
 from helicopter import Helicopter as Helico
+
 
 TICK_SLEEP = 0.05
 TREE_UPDATE = 50
@@ -13,14 +15,33 @@ field.generate_forest(3, 10)
 field.generate_river(10)
 field.generate_river(10)
 
-
 helico = Helico(MAP_W, MAP_H)
+
+
+
+MOVES = {'t':(-1, 0), 'h':(0, 1), 'g':(1, 0), 'f':(0, -1)}
+
+
+def procces_key(key):
+    global helico
+    c = key.char.lower()
+    if c in MOVES.keys():
+        dx, dy = MOVES[c][0], MOVES[c][1]
+        helico.move(dx, dy)
+
+listener = keyboard.Listener(
+    on_press=None,
+    on_release=procces_key,)
+listener.start()
+
 
 tick = 1
 
 while True:
     os.system("cls") # clear для linux
+    print(helico.x, helico.y)
     print("TICK", tick)
+    helico.print_menu()
     field.print_map(helico)
     tick += 1
     time.sleep(TICK_SLEEP)
