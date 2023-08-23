61d6d3b544c46a4e0ebb8e4e654385f871658290
Mephistos <Mephistos.user@yandex.ru>
2023-08-23 13:20:56 +0300

    upgrade bubble

--- Algoritmy_structury/1_Bubble.py
+++ Algoritmy_structury/1_Bubble.py
@@ -1,7 +1,7 @@
 # Сортировка пузырьком
 import random
 
-n = 20
+n = 10
 arr = list()
 for i in range(n):
     number = random.randint(1, 100)
@@ -12,19 +12,36 @@ print(arr)
 print("-------")
 
 ########################################################
+itter = n - 1
+count = 0
+is_sorted = True# флаг проверки были ли сделаны перестановки в текущем проходе
 for i in range(n):
-    for j in range(n - 1):
+    print(itter)
+    for j in range(itter):
         left = arr[j]
         rigth = arr[j + 1]
         if left > rigth:# если условие left < rigth то сортировка по убыванию 
             arr[j] = rigth
             arr[j + 1] = left
+            is_sorted = False# если изменения есть то флаг False
+    itter -= 1 # уменьшаем размер иттерации во вложенном цикле,
+               # т.к. крайнее правое значение по окончанию прохода проверять уже не нужно
+    print(arr)
+    if is_sorted is True:
+        break
+    else:
+        is_sorted = True
+
+
+    #        count += 1   
+    #if count == itter:
+     #   itter -= 1  
         # Еще один вариант:
         #if arr[j] > arr[j + 1]:
         #    temp = arr[j]
         #    arr[j] = arr[j + 1]
         #    arr[j + 1] = temp
 ########################################################
-
+print("-------")
 print("Sorted:")
 print(arr)
