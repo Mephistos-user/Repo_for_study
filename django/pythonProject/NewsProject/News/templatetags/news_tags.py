ffaea8e66bb9cf6beae382607e321e82ce46e3b4
Mephistos <Mephistos.user@yandex.ru>
2023-10-05 22:32:53 +0300

    рефакторинг сайта

--- django/pythonProject/NewsProject/News/templatetags/news_tags.py
+++ django/pythonProject/NewsProject/News/templatetags/news_tags.py
@@ -1,5 +1,6 @@
 from News.models import Category
 from django import template
+from django.db.models import Count
 
 register = template.Library()
 
@@ -9,5 +10,6 @@ def get_categories():
 
 @register.inclusion_tag('News/list_categories.html')
 def show_categories(arg1='Category', arg2='list'):
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
